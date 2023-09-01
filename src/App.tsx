30d0a273caede3024bf1f57f26a9f71c8e9abdcb
zpran <zanna@prane.org>
2023-09-01 16:29:45 +0100

    feat: task 5 - passing props

--- src/App.tsx
+++ src/App.tsx
@@ -89,8 +89,13 @@ function App(): JSX.Element {
 
       <main>
         <div className="cards__wrapper">
-          {cats.map((e) => (
-            <CatCard />
+          {cats.map((cat) => (
+            <CatCard
+              name={cat.name}
+              species={cat.species}
+              favFoods={cat.favFoods}
+              birthYear={cat.birthYear}
+            />
           ))}
         </div>
       </main>
